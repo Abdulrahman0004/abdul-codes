bf81d9dda756e7d572ec44070b4b587fd494bba7
Abdulrahman0004 <113187862+Abdulrahman0004@users.noreply.github.com>
2022-10-06 12:42:43 +0530

    Add files via upload

---
+++ C PROGRAM FILES/C_INPUT_MULVALUES.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+float b;
+clrscr();
+printf("Enter integer and then a float:");
+scanf("%d%f",&a,&b);
+printf("You entered %d and %f",a,b);
+getch();
+}
