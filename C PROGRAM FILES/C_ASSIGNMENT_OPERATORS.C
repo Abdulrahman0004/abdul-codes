4efbb6f7221942b9b6b4b29054930615ca7ab4b4
Abdulrahman0004 <113187862+Abdulrahman0004@users.noreply.github.com>
2022-10-06 14:03:10 +0530

    Add files via upload

---
+++ C PROGRAM FILES/C_ASSIGNMENT_OPERATORS.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a=5,c;
+clrscr();
+c=a;
+printf("c=%d\n",c);
+c+=a;
+printf("c=%d\n",c);
+c-=a;
+printf("c=%d\n",c);
+c*=a;
+printf("c=%d\n",c);
+c/=a;
+printf("c=%d\n",c);
+c%=a;
+printf("c=%d\n",c);
+getch();
+}
