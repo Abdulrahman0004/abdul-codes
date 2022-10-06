59668ae58e5d1346bbe046d98f982ceb75e59367
Abdulrahman0004 <113187862+Abdulrahman0004@users.noreply.github.com>
2022-10-06 12:41:31 +0530

    Add files via upload

---
+++ C PROGRAM FILES/C_INPUT_FLOAT.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char chr;
+clrscr();
+printf("Enter a character:");
+scanf("%c",&chr);
+printf("You entered: %c\n",chr);
+printf("ASCII value is: %d",chr);
+getch();
+}
