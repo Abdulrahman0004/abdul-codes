50c36fcd5d04c9b8db213964a83e43b2fadd2563
Abdulrahman0004 <113187862+Abdulrahman0004@users.noreply.github.com>
2022-10-06 11:28:52 +0530

    Add files via upload

---
+++ C PROGRAM FILES/C_INPUT_INT.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int testInteger;
+clrscr();
+printf("Enter an integer:");
+scanf("%d",&testInteger);
+printf("Number = %d",testInteger);
+getch();
+}
