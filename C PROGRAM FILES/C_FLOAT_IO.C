9c5a233b89454aa140904e46854799bc3c68dde6
Abdulrahman0004 <113187862+Abdulrahman0004@users.noreply.github.com>
2022-10-06 13:18:10 +0530

    Add files via upload

---
+++ C PROGRAM FILES/C_FLOAT_IO.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float num1;
+double num2;
+clrscr();
+printf("Enter a number:");
+scanf("%f",&num1);
+printf("Enter another number:");
+scanf("%lf",&num2);
+printf("num1 = %f\n",num1);
+printf("num2 = %f",num2);
+getch();
+}
