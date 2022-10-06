776c50bc92bcac60ee23e42eb562df146a9c81c7
Abdulrahman0004 <113187862+Abdulrahman0004@users.noreply.github.com>
2022-10-06 13:55:03 +0530

    Add files via upload

---
+++ C PROGRAM FILES/C_ASSIGNMENT_ORERATORS.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a=5,c;
+clrscr();
+c=a;
+printf("c=%d\n",c);
+c+=a;
+printf("c=%d\n",c);
+c*=a;
+printf("c=%d\n",c);
+c/=a;
+printf("c=%d\n",c);
+c=a%d;
+printf("c=%d\n",c);
+getch();
+}
