2a05de1178b9d01b67f819ad150890405c951bf4
Abdulrahman0004 <113187862+Abdulrahman0004@users.noreply.github.com>
2022-10-06 12:27:22 +0530

    Update C_INPUT_ASCII.C

--- C PROGRAM FILES/C_INPUT_ASCII.C
+++ C PROGRAM FILES/C_INPUT_ASCII.C
@@ -6,7 +6,7 @@ char chr;
 clrscr();
 printf("Enter a character:");
 scanf("%c",&chr);
-print("You entered: %c\n",chr);
+printf("You entered: %c\n",chr);
 printf("ASCII value is: %d",chr);
 getch();
 }
