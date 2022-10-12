62371d8f92b28d0fb115c233ad9ae243de3da9d3
Abdulrahman0004 <113187862+Abdulrahman0004@users.noreply.github.com>
2022-10-12 13:41:42 +0530

    Add files via upload

---
+++ C PROGRAM FILES/C_RELTWOINT.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int number1,number2;
+clrscr();
+printf("Enter two integers:");
+scanf("%d%d",&number1,&number2);
+if (number1==number2)
+{
+printf("Result: %d=%d",number1,number2);
+}
+else if (number1>number2)
+{
+printf("Result: %d<%d",number1,number2);
+}
+else
+{
+printf("Result:%d<%d",number1,number2);
+}
+getch();
+}
