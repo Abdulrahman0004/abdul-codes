4ea9ea82f147a87a02055c6b3daf8f8cb89176aa
Abdulrahman0004 <113187862+Abdulrahman0004@users.noreply.github.com>
2022-10-14 20:39:11 +0530

    Add files via upload

---
+++ C PROGRAM FILES/BreakAndContinue.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int i;
+double number,sum=0.0;
+for(i=1;i<=10;++i)
+{
+printf("Enter \n%d:",i);
+scanf("%lf",&number);
+if(number<0.0)
+{
+	break;
+}
+sum+=number;
+}
+printf("Sum=%2lf",sum);
+}
