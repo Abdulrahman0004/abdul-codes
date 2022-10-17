3af63f9d42664ae588c6fb3a0e0920f05223255d
Abdulrahman0004 <113187862+Abdulrahman0004@users.noreply.github.com>
2022-10-17 21:27:22 +0530

    Add files via upload

---
+++ C PROGRAM FILES/cIfElseLadder.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+double num;
+printf("Enter a number:");
+scanf("%lf",&num);
+if(num<0.0)
+ printf("You entered a negative number");
+else if(num>0.0)
+ printf("You entered a positive number");
+else
+ printf("You entered 0");
+}
