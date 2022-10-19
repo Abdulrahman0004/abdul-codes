328f7e253c4a51cb17b2bbb240ee96a75bc16427
Abdulrahman0004 <113187862+Abdulrahman0004@users.noreply.github.com>
2022-10-19 20:38:58 +0530

    Add files via upload

---
+++ C PROGRAM FILES/cCountNumOfDigits.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{ 
+long long n;
+int count=0;
+printf("Enter an integer:");
+scanf("%1ld",&n);
+do
+{
+n/=10;
+++count;
+}while(n!=0);
+printf("Number of digits:%d",count);
+}
