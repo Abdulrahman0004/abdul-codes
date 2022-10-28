b388987ab1671e5f1c96048006ed4f7d3296863f
Abdulrahman0004 <113187862+Abdulrahman0004@users.noreply.github.com>
2022-10-28 17:25:15 +0530

    Add files via upload

---
+++ JAVA PROGRAM FILES/SumAverageRunningInt.java
@@ -0,0 +1,18 @@
+package partiallyOpenlabbook;
+
+public class SumAverageRunningInt {
+
+	public static void main(String[] args) {
+		int sum=0;
+		double average;
+		final int LOWERBOUND=1;
+		final int UPPERBOUND=100;
+		for(int number=LOWERBOUND;number<=UPPERBOUND;++number)
+		{
+			sum+=number;
+		}
+        System.out.println("The sum of 1 to 100 is:"+sum);
+        average=sum/UPPERBOUND;
+        System.out.println("The average is:"+average);
+	}
+}
